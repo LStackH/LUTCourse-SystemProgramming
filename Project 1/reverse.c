f089d838d6bbc791ece9d5d31958f9edcdda285c
Lukas Honka <lhonka0@gmail.com>
2024-11-20 18:55:47 +0200

    completed functionality, linked list, edge cases

--- Project 1/reverse.c
+++ Project 1/reverse.c
@@ -2,6 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct Node
+{
+    char *data;
+    struct Node *pNext;
+} Node;
+
+// add new nodes to the linked list
+Node *addToBeginning(Node *head, char *str)
+{
+    Node *pNew = (Node *)malloc(sizeof(Node));
+    if (!pNew)
+    {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+    pNew->data = malloc(strlen(str) + 1);
+    if (!pNew->data)
+    {
+        fprintf(stderr, "malloc failed\n");
+        exit(1);
+    }
+    strcpy(pNew->data, str);
+    pNew->pNext = head; // Add at the beginning
+    return pNew;
+}
+
+void freeList(Node *head)
+{
+    // go through the list and free each node
+    while (head)
+    {
+        Node *temp = head;
+        head = head->pNext;
+        free(temp->data);
+        free(temp);
+    }
+}
+
+void printOrWrite(Node *head, FILE *outputFile)
+{
+    Node *current = head;
+    while (current)
+    {
+        fprintf(outputFile, "%s", current->data);
+        current = current->pNext;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     if (argc > 3)
@@ -12,56 +60,72 @@ int main(int argc, char *argv[])
 
     FILE *inputFile = NULL;
     FILE *outputFile = NULL;
-    char str[50];
+    Node *head = NULL;
 
-    // 2 args, reverse input
     if (argc == 2)
     {
         inputFile = fopen(argv[1], "r");
         if (!inputFile)
         {
-            fprintf(stderr, "error: cannot open file %s'\n'", argv[1]);
+            fprintf(stderr, "error: cannot open file '%s'\n", argv[1]);
+            exit(1);
+        }
+        outputFile = stdout;
+    }
+    else if (argc == 3)
+    {
+        // Check if input and output files are the same before opening the output file
+        if (strcmp(argv[1], argv[2]) == 0)
+        {
+            fprintf(stderr, "Input and output file must differ\n");
             exit(1);
         }
 
-        while (fgets(str, 50, inputFile) != NULL)
-        {
-            fprintf(stdout, "%s", str);
-        }
-        printf("\n");
-        fclose(inputFile);
-    }
-    // 3 args, reverse input output
-    else if (argc == 3)
-    {
         inputFile = fopen(argv[1], "r");
         if (!inputFile)
         {
-            fprintf(stderr, "error: cannot open file %s'\n'", argv[1]);
+            fprintf(stderr, "error: cannot open file '%s'\n", argv[1]);
             exit(1);
         }
         outputFile = fopen(argv[2], "w");
         if (!outputFile)
         {
-            fprintf(stderr, "error: cannot open file %s'\n'", argv[1]);
+            fprintf(stderr, "error: cannot open file '%s'\n", argv[2]);
             fclose(inputFile);
             exit(1);
         }
-        if (strcmp(argv[1], argv[2]) == 0)
-        {
-            fprintf(stderr, "Input and output file must differ\n");
-            fclose(inputFile);
-            fclose(outputFile);
-            exit(1);
-        }
-
-        while (fgets(str, 50, inputFile) != NULL)
-        {
-            fprintf(outputFile, "%s", str);
-        }
-        fclose(inputFile);
-        fclose(outputFile);
     }
+    else
+    {
+        inputFile = stdin;
+        outputFile = stdout;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+
+    while (getline(&line, &len, inputFile) != -1)
+    {
+        // if we use keyword exit, we can exit manual input
+        if (strcmp(line, "exit\n") == 0)
+        {
+            fprintf(stderr, "Exiting program...\n");
+            break;
+        }
+        head = addToBeginning(head, line);
+    }
+
+    free(line);
+
+    printOrWrite(head, outputFile);
+
+    if (inputFile != stdin)
+        fclose(inputFile);
+    if (outputFile != stdout)
+        fclose(outputFile);
+
+    // free the linked list
+    freeList(head);
 
     return 0;
 }
